831c317331ddbfc3a08b5738127da5f7d72a2933
arturnowakowski2015 <arturnowakowski2015@gmail.com>
2023-06-20 18:34:36 +0200

    search box

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -4,7 +4,7 @@ const useSearch = () => {
   const [options, setOptions] = useState<string[]>([""]);
   const [query, setQuery] = useState("");
   const filteredOptions = (str: string) => {
-    let opt: string[] = data.filter((t) => t.indexOf(str) !== -1);
+    let opt: string[] = data.filter((t) => t.indexOf(str) !== -1 && t);
     if (options.length === 0) data.splice(0, 0, str);
     setOptions(opt);
   };
